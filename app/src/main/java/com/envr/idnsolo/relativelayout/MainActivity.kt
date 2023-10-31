7b52009f4883179db2f74cd19901c52916373688
zlibdev <enver.avisena@gmail.com>
2023-10-31 09:26:03 +0700

    first commit

---
+++ app/src/main/java/com/envr/idnsolo/relativelayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.envr.idnsolo.relativelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
